92da9df0b8d217b538de91ca87b96d9923c6758e
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-09 15:41:29 -0300

    Files

--- web/Spring/2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ web/Spring/2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.FieldNamingPolicy;
@@ -21,53 +24,67 @@ public class PrincipalComBusca {
 	public static void main(String[] args) throws IOException, InterruptedException {
 
 		Scanner leitura = new Scanner(System.in);
-		System.out.println("Digite o nome da obra: ");
-		var tituloBusca = leitura.nextLine();
-
-		tituloBusca = tituloBusca.replace(" ", "+");
-		String address = "https://www.omdbapi.com/?t=" + tituloBusca + "&apikey=cd70d0b3";
+		var tituloBusca = "";
 		
-		try {
+		List<Titulo> titulos = new ArrayList<Titulo>();
+		
 
-			HttpClient client = HttpClient.newHttpClient();
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create(address)).build();
+		while (true) {
 
-			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+			System.out.println("Digite o nome da obra: ");
+			tituloBusca = leitura.nextLine();
 
-			String json = response.body();
-			System.out.println("JSON: " + json);
+			if (tituloBusca.equalsIgnoreCase("Exit")) {
+				break;
+			}
+			
+			tituloBusca = tituloBusca.replace(" ", "+");
+			String address = "https://www.omdbapi.com/?t=" + tituloBusca + "&apikey=cd70d0b3";
+
+			try {
+
+				HttpClient client = HttpClient.newHttpClient();
+				HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
+
+				HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+				String json = response.body();
+				System.out.println("JSON: " + json);
 
 //			Gson gson = new Gson();
 //			Titulo meuTitulo = gson.fromJson(json, Titulo.class);
 //			System.out.println("Título: " + meuTitulo.getNome());
 //			System.out.println(meuTitulo);
 
-			Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+				Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
-			TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
-			System.out.println("Record: " + meuTituloOMDB);
+				TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
+				System.out.println("Record: " + meuTituloOMDB);
 
-//		try {
-			Titulo meuTitulo = new Titulo(meuTituloOMDB);
-			System.out.println("meuTitulo: " + meuTitulo);
-		} catch (NumberFormatException ex) {
-			System.out.println("An error occured: " + ex.getMessage());
-		}catch(IllegalArgumentException ex) {
-			System.out.println("An error occured: " + ex.getMessage());
+				Titulo meuTitulo = new Titulo(meuTituloOMDB);
+				System.out.println("Titulo convertido: " + meuTitulo);
+
+				titulos.add(meuTitulo);
+				
+//				FileWriter writting = new FileWriter("movies.txt");
+//				writting.write(tituloBusca);
+//				writting.close();
+
+			} catch (NumberFormatException ex) {
+				System.out.println("An error occured: " + ex.getMessage());
+			} catch (IllegalArgumentException ex) {
+				System.out.println("An error occured: " + ex.getMessage());
 //			ex.printStackTrace();
-		}catch(ErrorYearConversionException ex) {
-			System.out.println("An error occured: " + ex.getMessage());
+			} catch (ErrorYearConversionException ex) {
+				System.out.println("An error occured: " + ex.getMessage());
+			} catch (NullPointerException | IllegalStateException ex) {
+				System.out.println("Multicatch example: " + ex.getMessage());
+			} finally {
+				System.out.println("The program finished successfully");
+			}
+			
 		}
-		catch(NullPointerException | IllegalStateException ex) {
-			System.out.println("Multicatch example: " + ex.getMessage());
-		}
-		finally {
-			System.out.println("The program finished successfully");
-		}
-		
-//		System.exit(0);
-
+		System.out.println(titulos);
 	}
 
 }
