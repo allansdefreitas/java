163d650e17ad7076d1df0d72267b3897bf531854
allansdefreitas <allan.allan.santos@gmail.com>
2025-07-28 15:53:28 -0300

    Kafka with java

---
+++ alura/kafka/e-commerce-kafka/src/main/java/br/edu/allan/ecommerce/kafka/NewOrderMain.java
@@ -0,0 +1,50 @@
+package br.edu.allan.ecommerce.kafka;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    private final static String NOME_TOPICO = "ECOMMERCE_NEW_ORDER";
+    public static void main(String[] args) {
+
+        var producer = new KafkaProducer<String, String>(properties());
+
+        var value = "12345,85458,784521514";
+
+        var record = new ProducerRecord<String, String>(NOME_TOPICO, value, value);
+
+        try {
+            producer.send(record, (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+            }).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static Properties properties() {
+
+        var properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+}
