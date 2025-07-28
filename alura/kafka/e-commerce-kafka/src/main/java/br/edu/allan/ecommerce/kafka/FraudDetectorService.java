2d7448ce6871f79870c2e1c04ba40c4f2a7a663f
allansdefreitas <allan.allan.santos@gmail.com>
2025-07-28 16:52:03 -0300

    kafka com Java

---
+++ alura/kafka/e-commerce-kafka/src/main/java/br/edu/allan/ecommerce/kafka/FraudDetectorService.java
@@ -0,0 +1,57 @@
+package br.edu.allan.ecommerce.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+    private final static String NOME_TOPICO = "ECOMMERCE_NEW_ORDER";
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList(NOME_TOPICO));
+        var records = consumer.poll(Duration.ofMillis(100));
+
+        if(records.isEmpty()){
+            System.out.println("There is not records");
+        }else{
+            for(var record : records){
+                System.out.println("-------------------------------------------");
+                System.out.println("processing new order, checking for fraud");
+                System.out.println(record.key());
+                System.out.println(record.value());
+                System.out.println(record.partition());
+                System.out.println(record.offset());
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Order processed!");
+
+            }
+        }
+                
+    }
+
+    private static Properties properties() {
+
+        var properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        return properties;
+    }
+
+
+}
