224f76842cfb41290d12ddfb79e739d161420e09
allansdefreitas <allan.allan.santos@gmail.com>
2025-07-31 15:26:25 -0300

    Kafka: MAX POLL RECORDS

--- alura/kafka/e-commerce-kafka/src/main/java/br/edu/allan/ecommerce/kafka/FraudDetectorService.java
+++ alura/kafka/e-commerce-kafka/src/main/java/br/edu/allan/ecommerce/kafka/FraudDetectorService.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
     private final static String TOPIC_ECOMMERCE_NEW_ORDER = "ECOMMERCE_NEW_ORDER";
@@ -53,6 +54,8 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
 
         return properties;
     }
